221d1a42feb4f5cc405cdbc37079fae20bbe3598
sofiaff <fassi_fehri.sofia@courrier.uqam.ca>
2022-02-24 00:06:23 -0500

    Debut de la creation de la fonction d'upload du fichier tsv

--- app.R
+++ app.R
@@ -1,4 +1,25 @@
+options(shiny.maxRequestSize = 10 * 1024^2)
 library(shiny)
-runExample("01_hello")
+ui <- fluidPage(
+  fileInput("upload", NULL, accept = c(".csv", ".tsv")),
+  numericInput("n", "Number of Rows you want to display", value = 5, min = 1, step = 1),
+  tableOutput("head")
+)
 
-
+server <- function(input, output, session) {
+  data <- reactive({
+    req(input$upload)
+    
+    ext <- tools::file_ext(input$upload$name)
+    switch(ext,
+           csv = vroom::vroom(input$upload$datapath, delim = ","),
+           tsv = vroom::vroom(input$upload$datapath, delim = "\t"),
+           validate("Invalid file; Please upload a .csv or .tsv file")
+    )
+  })
+  
+  output$head <- renderTable({
+    head(data(), input$n)
+  })
+}
+shinyApp(ui, server)
