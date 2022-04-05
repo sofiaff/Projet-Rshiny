7de4d56f98fc2926f2e28f146534b0ceee93452f
sofiaff <fassi_fehri.sofia@courrier.uqam.ca>
2022-04-04 22:16:19 -0400

    Ajout du PCA plot

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 options(shiny.maxRequestSize = 10 * 1024^2)
-library(ggplot2) 
+library("ggplot2") 
+library("ggfortify")
 shinyApp(
   ui = tagList(
     #shinythemes::themeSelector(),
@@ -21,7 +22,9 @@ shinyApp(
                             h4("vulcano Plot"),
                             plotOutput("outputVulcano")
                             ),
-                   tabPanel("Tab 3", "This panel is intentionally left blank")
+                   tabPanel("PCA", 
+                            h4("PCA plot"),
+                            plotOutput("PCAPlot"))
                  )
                )
       ),
@@ -31,16 +34,22 @@ shinyApp(
     output$table <- renderDataTable({
       file <- input$file1
       read.csv(file$datapath, header = input$header)
-      #tmp<-read.csv(file$datapath, header = input$header)
     })
     output$outputVulcano <- renderPlot({
       file<- input$file1
-      ggplot(data=read.csv(file$datapath, header = input$header), aes(x=log2FoldChange, y=-log10(pvalue))) + 
+      cleanfile<-na.omit(read.csv(file$datapath, header = input$header))
+      ggplot(data=cleanfile, aes(x=log2FoldChange, y=-log10(pvalue))) + 
         geom_point() + 
         theme_minimal()+
         scale_color_manual(values=c("blue", "black", "red")) +
         geom_vline(xintercept=c(-0.6, 0.6), col="red") +
         geom_hline(yintercept=-log10(0.05), col="red")
+    })
+    output$PCAPlot <- renderPlot({
+      file<- input$file1
+      data<-na.omit(read.csv(file$datapath, header = input$header))
+      pca<-prcomp(data[,5:10])
+      autoplot(pca)
     })
   }
 )
