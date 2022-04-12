687230601ea89e7c36f052fa647dcb4cdbdda46c
sofiaff <fassi_fehri.sofia@courrier.uqam.ca>
2022-04-12 00:15:53 -0400

    Amélioration du template et volcano plot

--- app.R
+++ app.R
@@ -4,29 +4,49 @@ library("ggfortify")
 shinyApp(
   ui = tagList(
     #shinythemes::themeSelector(),
-    navbarPage(
+    navbarPage("Projet BIF7104",
       # theme = "cerulean",  # <--- To use a theme, uncomment this
       #"shinythemes",
-      tabPanel("Projet BIF7104",
+      tabPanel("Table de données",
+               h4("Table"),
+               dataTableOutput("table"),
                sidebarPanel(
                  fileInput("file1", "Choose CSV File", accept = ".csv"),
                  checkboxInput("header", "Header", TRUE),
-               ),
-               mainPanel(
-                 tabsetPanel(
-                   tabPanel("Table de donnees",
-                            h4("Table"),
-                            dataTableOutput("table"),
-                   ),
-                   tabPanel("VolcanoPlot", 
-                            h4("vulcano Plot"),
-                            plotOutput("outputVulcano")
-                            ),
-                   tabPanel("PCA", 
-                            h4("PCA plot"),
-                            plotOutput("PCAPlot"))
-                 )
                )
+      ),
+      tabPanel("Volcano Plot",
+               h4("Volcano Plot"),
+               plotOutput("volcano_plot",
+                          width = "70%",
+                          height = "600px",
+                          hover = "volcano_hover",
+                          click = "volcano_click",
+                          dblclick = "volcano_dbl_click",
+                          brush = brushOpts(
+                            id = "volcano_brush",
+                            resetOnNew = TRUE)),
+               sidebarPanel(
+                 sliderInput("pvalue_threshold",
+                             "Set significance threshold",
+                             min = 0,
+                             max = 5,
+                             value = .05),
+               )
+      ),
+      
+      tabPanel("PCA", 
+               h4("PCA plot"),
+               plotOutput("PCAPlot")
+      ),
+      tabPanel("Heatmap",
+             h4("Heatmap"),
+             plotOutput("HeatmapPlot")
+      ),
+      tabPanel("Gene Ontology",
+             h4("Gene"),
+             plotOutput('Gene plot')
+     
       ),
     )
   ),
@@ -35,14 +55,14 @@ shinyApp(
       file <- input$file1
       read.csv(file$datapath, header = input$header)
     })
-    output$outputVulcano <- renderPlot({
+    output$volcano_plot <- renderPlot({
       file<- input$file1
       cleanfile<-na.omit(read.csv(file$datapath, header = input$header))
       ggplot(data=cleanfile, aes(x=log2FoldChange, y=-log10(pvalue))) + 
         geom_point() + 
         theme_minimal()+
         scale_color_manual(values=c("blue", "black", "red")) +
-        geom_vline(xintercept=c(-0.6, 0.6), col="red") +
+        geom_vline(xintercept=c(-input$pvalue_threshold, input$pvalue_threshold), col="red") +
         geom_hline(yintercept=-log10(0.05), col="red")
     })
     output$PCAPlot <- renderPlot({
